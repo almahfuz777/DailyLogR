461848f26566263a13afb1c9ca90898741ef7fa1
Al Mahfuz <almahfuz777@gmail.com>
2025-11-28 06:39:02 +0600

    Integrate Firebase and to project
    - Added Firebase initialization to main.dart and
    - included firebase_core, firebase_auth, cloud_firestore, and google_sign_in dependencies.
    - Updated Gradle files to apply Google services plugin and added google-services.json for Android configuration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:dailylogr/screens/main_screen.dart';
 import 'services/hive_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   // Hive initialization
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp();
   await HiveService.init();
 
   // Run the app
