5c5852e2c6a2c3c72d8446f13a36e5ac68929c8d
Al Mahfuz <almahfuz777@gmail.com>
2026-06-09 02:56:56 +0600

    Set local timezone via flutter_timezone
    Ensure scheduled notifications use the device timezone by initializing timezone data and setting the local tz location from flutter_timezone.getLocalTimezone(). Adds flutter_timezone dependency and import, and wraps the location set in a try/catch with debugPrint on failure.

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:dailylogr/services/hive_service.dart';
@@ -30,6 +31,12 @@ class NotificationService {
 
   Future<void> init() async {
     tz.initializeTimeZones();
+    try {
+      final String timeZoneName = (await FlutterTimezone.getLocalTimezone()).identifier;
+      tz.setLocalLocation(tz.getLocation(timeZoneName));
+    } catch (e) {
+      debugPrint("Failed to set local timezone: \$e");
+    }
 
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/launcher_icon');
